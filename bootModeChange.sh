af0b1bbd404514fa2e05ec6fa6fcfaf9c7e1bd97
boyanhristov96 <bxhristov@students.everettcc.edu>
2018-08-15 17:34:32 -0700

    fixed a bug in the script

--- bootModeChange.sh
+++ bootModeChange.sh
@@ -7,7 +7,7 @@ if [[ $1 -eq '1' ]]
 	then
 	read -p "Do you want to boot in console mode?[y/n]" -n 1
 	echo
-	if [[ $REPLY=~[$yY] ]]
+	if [[ $REPLY =~ [$yY] ]]
 		then
 		# this should be just the console
 		systemctl set-default multi-user.target
@@ -18,7 +18,7 @@ if [[ $1 -eq '2' ]]
 	then
 	read -p "Do you want to boot in normal graphical mode?[y/n]" -n 1
 	echo
-	if [[ $REPLY=~[$yY] ]]
+	if [[ $REPLY =~ [$yY] ]]
 		then
 		# activates regular login
 		systemctl set-default graphical.target
@@ -29,9 +29,20 @@ if [[ $1 -eq '3' ]]
 	then
 	read -p "Do you want to start the graphic user interface right away?[y/n]" -n 1
 	echo
-	if [[ $REPLY=~[$yY] ]]
+	if [[ $REPLY =~ [$yY] ]]
 		then
 		# start the gui right away
 		/etc/init.d/lightdm start 
+	fi
+fi
+
+if [[ $1 -eq '7' ]]
+	then 
+	read -p "Are you sure that you want to enter console mode? Not recommended. Press Ctrl+Alt+F1 once screen goes black[y/n]" -n 1
+	echo
+	if [[ $REPLY =~ [$yY] ]]
+		then
+		# stop the gui right away
+		/etc/init.d/lightdm stop
 	fi
 fi
